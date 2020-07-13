44af4d5233d58ae8a36dc958032eab1b192de3db
Armando Reyes <risk.management.aseuam@gmail.com>
2020-07-13 03:24:23 -0500

    Attempt Assignment 03

--- redux/comments.js
+++ redux/comments.js
@@ -9,6 +9,8 @@ export const comments = ( state = {
                 return{...state,errMess:null, comments: action.payload};
             case ActionTypes.COMMENTS_FAILED:
                 return{...state,errMess:action.payload, comments: []};
+            case ActionTypes.ADD_COMMENT:
+                return(...state, errMess: )
             default:
                 return state;
         }
