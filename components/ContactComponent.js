fca4996b5ce159f57b299dcb371565270af7d4f6
Armando Reyes <risk.management.aseuam@gmail.com>
2020-07-19 00:43:40 -0500

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,32 +1,21 @@
 import React,{ Component } from 'react';
 import { ScrollView, Text} from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card,Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 
-function RenderItem () {
-   
-        return(
-            <Animatable.View animation = "fadeInDown" duration ={2000} delay= {1000}>
-                <Card
-                    
-                    title = 'Contact Information'
-                    >
-                    <Text style = {{margin:10}}>
-                        121, Clear Water Bay Road  {"\n"} {"\n"}
-                        Clear Water Bay, Kowloon {"\n"} {"\n"}
-                        HONG KONG {"\n"} {"\n"}
-                        Tel: +852 1234 5678 {"\n"} {"\n"}
-                        Fax: +852 8765 4321 {"\n"} {"\n"}
-                        Email:confusion@food.net  
-                    </Text>
-                </Card>
-            </Animatable.View>
-        );
-    
-}
+
 
 class Contact extends Component {
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern: '
+        });       
+    }
 
     static navigationOptions = {
         title: 'Contact Us'
@@ -35,7 +24,26 @@ class Contact extends Component {
     render (){
         return (
             <ScrollView>
-                <RenderItem/>
+                <Animatable.View animation = "fadeInDown" duration ={2000} delay= {1000}>
+                    <Card
+                        title = 'Contact Information'
+                        >
+                        <Text style = {{margin:10}}>
+                            121, Clear Water Bay Road  {"\n"} {"\n"}
+                            Clear Water Bay, Kowloon {"\n"} {"\n"}
+                            HONG KONG {"\n"} {"\n"}
+                            Tel: +852 1234 5678 {"\n"} {"\n"}
+                            Fax: +852 8765 4321 {"\n"} {"\n"}
+                            Email:confusion@food.net  
+                        </Text>
+                        <Button
+                        title = "Send Email"
+                        buttonStyle ={{backgroundColor : "#512DA8"}}
+                        icon = {<Icon name ='envelope-o' type ='font-awesome' color ='white' />}
+                        onPress = {this.sendMail}
+                        />
+                    </Card>
+                </Animatable.View>  
             </ScrollView>
         );
     }
